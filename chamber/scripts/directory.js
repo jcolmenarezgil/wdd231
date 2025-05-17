ceea6ede06233affe9c2eedbe7f3c0f0b1f3224b
jcolmenarezgil <jcolmenarezgil@byupathway.edu>
2025-05-16 21:50:45 -0400

    Chamber Members Update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,4 @@
-const url = 'https://jcolmenarezgil.github.io/wdd231/chamber/chamber-directory.json';
+const url = 'https://jcolmenarezgil.github.io/wdd231/chamber/data/members.json';
 const cards = document.querySelector('#cards');
 
 async function getCompanyData() {
@@ -16,11 +16,16 @@ const displaycompanys = (companys) => {
         let companyName = document.createElement('h3');
         let phone = document.createElement('p');
         let email = document.createElement('p');
+        let address = document.createElement('p');
+        let website = document.createElement('p');
         let portrait = document.createElement('img');
 
         companyName.textContent = `${company.company_name}`;
         phone.textContent = `Phone: ${company.phone}`;
+        address.textContent = `Address: ${company.address}`;
+        website.textContent = `Website: ${company.website}`;
         email.textContent = `Email: ${company.email}`;
+
         portrait.setAttribute('src', company.imageurl);
         portrait.setAttribute('class', 'company-portrait');
         portrait.setAttribute('alt', `Portrait of ${company.company_name}`);
@@ -31,6 +36,8 @@ const displaycompanys = (companys) => {
         card.appendChild(companyName);
         card.appendChild(phone);
         card.appendChild(email);
+        card.appendChild(address);
+        card.appendChild(website);
         card.appendChild(portrait);
 
         cards.appendChild(card);
